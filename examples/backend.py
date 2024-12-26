0a3b16ae72b160a019d2e51cd63c7bf67d361323
Marius Facktor <mariusfacktor@gmail.com>
2024-12-25 21:17:45 -0800

    added example backend and client

---
+++ examples/backend.py
@@ -0,0 +1,14 @@
+# backend.py
+from forthright import forthright_server
+from flask import Flask
+
+app = Flask(__name__)
+frs = forthright_server(app)
+
+def add_and_sub(numA, numB):
+    return numA + numB, numA - numB
+
+frs.export_functions(add_and_sub)
+
+if __name__ == '__main__':
+    app.run(port=8000)
---
+++ examples/client.py
@@ -0,0 +1,9 @@
+# client.py
+from forthright import forthright_client
+
+url = 'http://127.0.0.1:8000'
+frc = forthright_client(url)
+frc.import_functions('add_and_sub')
+
+sum, diff = frc.add_and_sub(8, 2)
+print('%d %d' %(sum, diff)) # -> 10 6
