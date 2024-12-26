5214896217680937ad9e6f5086ec4d096e87caf9
Marius Facktor <mariusfacktor@gmail.com>
2024-12-25 21:13:48 -0800

    added Safe Mode to send with json instead of serialized

--- tests/client.py
+++ tests/client.py
@@ -9,8 +9,9 @@ from src.forthright.forthright import forthright_client
 # from forthright import forthright_client
 
 url = 'http://127.0.0.1:8000'
+b_safe_mode = False
 
-frc = forthright_client(url)
+frc = forthright_client(url, safe_mode=b_safe_mode)
 frc.import_functions('calculate_values', 'concat_words', 'increment_age', 'optional_input', 'input_list')
 
 
@@ -24,15 +25,16 @@ concat_output = frc.concat_words(word1='aaa', word2='bbb', word3='ccc')
 print(concat_output) # -> aaabbbccc
 
 
-# test3 -- input and output are a custom object
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+if not b_safe_mode:
+	# test3 -- input and output are a custom object
+	class Person:
+	    def __init__(self, name, age):
+	        self.name = name
+	        self.age = age
 
-p1 = Person('john', 38)
-older_person = frc.increment_age(p1)
-print(older_person.age) # -> 39
+	p1 = Person('john', 38)
+	older_person = frc.increment_age(p1)
+	print(older_person.age) # -> 39
 
 
 # test4 -- zero input arguments
